f047353781c0975f9c202d6d1af00f9389a2bc5d
Terry.Mao <83873308@qq.com>
2014-04-19 14:53:12 +0800

    init repo

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/golang/glog"
+	"runtime"
+)
 
+func main() {
+	flag.Parse()
+	glog.Errorf("gosnowflake service start")
+	sc := InitSignal()
+	defer glog.Flush()
+	// config
+	if err := InitConfig(); err != nil {
+		glog.Errorf("InitConfig() error(%v)", err)
+		return
+	}
+	runtime.GOMAXPROCS(MyConf.MaxProc)
+	// process
+	if err := InitProcess(); err != nil {
+		glog.Errorf("InitProcess() error(%v)", err)
+		return
+	}
+	// pprof
+	InitPprof()
+	if err := InitRPC(); err != nil {
+		glog.Errorf("InitRPC() error(%v)", err)
+		return
+	}
+	// init signals, block wait signals
+	HandleSignal(sc)
+	glog.Errorf("gosnowflake service stop")
 }
