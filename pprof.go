2fadb0336c42e6b72400e318ba3be1cb3cb69723
Terry.Mao <83873308@qq.com>
2014-04-19 14:53:22 +0800

    add pprof

---
+++ pprof.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/golang/glog"
+	"net/http"
+	"net/http/pprof"
+)
+
+// StartPprof start http pprof.
+func InitPprof() {
+	pprofServeMux := http.NewServeMux()
+	pprofServeMux.HandleFunc("/debug/pprof/", pprof.Index)
+	pprofServeMux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	pprofServeMux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	pprofServeMux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	for _, addr := range MyConf.PprofBind {
+		go func() {
+			if err := http.ListenAndServe(addr, pprofServeMux); err != nil {
+				glog.Errorf("http.ListenAndServe(\"%s\", pproServeMux) error(%v)", addr)
+				panic(err)
+			}
+		}()
+	}
+}
