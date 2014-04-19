295785d313d497768c879f49ea85b67a16cbabdd
Terry.Mao <83873308@qq.com>
2014-04-19 14:53:59 +0800

    init repo

--- zk.go
+++ zk.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/golang/glog"
 	"github.com/samuel/go-zookeeper/zk"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -30,7 +33,7 @@ func InitZK() (*zk.Conn, error) {
 }
 
 // RegWorkerId register the workerid in zookeeper, check exists or not to avoid the duplicate workerid.
-func RegWorkerId(conn *zk.Conn, int workerId) error {
+func RegWorkerId(conn *zk.Conn, workerId int64) error {
 	glog.Infof("trying to claim workerId: %d", workerId)
 	zkPath := fmt.Sprintf("%s/%d", MyConf.ZKPath)
 	// retry
@@ -42,7 +45,7 @@ func RegWorkerId(conn *zk.Conn, int workerId) error {
 				time.Sleep(regRetrySecond)
 			} else {
 				glog.Errorf("zk.create(\"%s\") error(%v)", zkPath, err)
-				return nil, err
+				return err
 			}
 		} else {
 			return nil
@@ -52,42 +55,62 @@ func RegWorkerId(conn *zk.Conn, int workerId) error {
 }
 
 // peers get workers all children in zookeeper.
-func peers(conn *zk.Conn) (map[int]string, error) {
-    // try create ZKPath
-    _, err := conn.Create(MyConf.ZKPath, []byte(""), 0, zk.WorldACL(zk.PermAll))
-    if err != nil {
-        if err == zk.ErrNodeExists {
-            glog.Warningf("zk.create(\"%s\") exists", MyConf.ZKPath)
-        } else {
-            glog.Errorf("zk.create(\"%s\") error(%v)", MyConf.ZKPath, err)
-            return nil, err
-        }
+func peers(conn *zk.Conn) (map[int][]string, error) {
+	// try create ZKPath
+	_, err := conn.Create(MyConf.ZKPath, []byte(""), 0, zk.WorldACL(zk.PermAll))
+	if err != nil {
+		if err == zk.ErrNodeExists {
+			glog.Warningf("zk.create(\"%s\") exists", MyConf.ZKPath)
+		} else {
+			glog.Errorf("zk.create(\"%s\") error(%v)", MyConf.ZKPath, err)
+			return nil, err
+		}
 	}
-    // fetch all nodes
+	// fetch all nodes
 	workers, _, err := conn.Children(MyConf.ZKPath)
 	if err != nil {
-        glog.Errorf("zk.Get(\"%s\") error(%v)", MyConf.ZKPath, err)
-        return nil, err
+		glog.Errorf("zk.Get(\"%s\") error(%v)", MyConf.ZKPath, err)
+		return nil, err
 	}
-    res := make(map[int]string, len(workers))
-    for _, worker := range workers {
-        workerId, err := strconv.Atoi(worker)
-        if err != nil {
-            glog.Errorf("strconv.Atoi(\"%s\") error(%v)", worker, err)
-            return nil, err
-        }
-        // get info
-        zkPath := fmt.Sprintf("%s/%s", MyConf.ZKPath, worker)
-        d, _, err := conn.Get(zkPath)
-        if err != nil {
-            glog.Errorf("zk.Get(\"%s\") error(%v)", zkPath, err)
-            return nil, err
-        }
-        res[workerId] = string(d)
-    }
-    return res, nil
+	res := make(map[int][]string, len(workers))
+	for _, worker := range workers {
+		id, err := strconv.Atoi(worker)
+		if err != nil {
+			glog.Errorf("strconv.Atoi(\"%s\") error(%v)", worker, err)
+			return nil, err
+		}
+		// get info
+		zkPath := fmt.Sprintf("%s/%s", MyConf.ZKPath, worker)
+		d, _, err := conn.Get(zkPath)
+		if err != nil {
+			glog.Errorf("zk.Get(\"%s\") error(%v)", zkPath, err)
+			return nil, err
+		}
+		res[id] = strings.Split(string(d), ",")
+	}
+	return res, nil
 }
 
 // sanityCheckPeers check the zookeeper datacenterId and all nodes time.
-func sanityCheckPeers() error {
+func sanityCheckPeers(conn *zk.Conn) error {
+	allPeers, err := peers(conn)
+	if err != nil {
+		glog.Errorf("peers() error(%v)", err)
+		return err
+	}
+	//timestamps := int64(0)
+	for id, addrs := range allPeers {
+		if len(addrs) == 0 {
+			glog.Warningf("peers: %d don't have any address", id)
+			continue
+		}
+		// use first addr
+		// rpc
+		// check datacenterid
+		// check workerid
+		// add timestamps
+	}
+	// calc avg timestamps
+	// check 10s
+	return nil
 }
